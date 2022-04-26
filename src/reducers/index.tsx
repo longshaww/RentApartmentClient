a11752296708d8766a6b504ba7d3c3c95b38cb67
longshaww <at400123@gmail.com>
2022-04-27 05:32:09 +0700

    sprint2 client resolve #11

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -3,12 +3,14 @@ import { detailLessorReducer } from "./detail.lessor";
 import { listLessorReducer } from "./lessor";
 import { detailApartmentReducer } from "./detail.apartment";
 import { dateChoosenReducer } from "./date.choosen";
+import { billInformationReducer } from "./bill.information";
 
 const rootReducer = combineReducers({
 	detailLessor: detailLessorReducer,
 	listLessor: listLessorReducer,
 	detailApartment: detailApartmentReducer,
 	dateChoosen: dateChoosenReducer,
+	billInformation: billInformationReducer,
 });
 
 export default rootReducer;
